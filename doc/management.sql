786f101fd931ce0f7cababe997e34f1b7f141b2e
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2022-12-28 20:22:31 +0900

    modifying the configuration files

---
+++ doc/management.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- ホスト: mysql
+-- 生成日時: 2022 年 3 月 09 日 07:39
+-- サーバのバージョン： 8.0.25
+-- PHP のバージョン: 7.4.20
+
+-- SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+-- START TRANSACTION;
+-- SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- データベース: ``
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 ``
+--
+
+
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 ``
+--
+
+
+
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス ``
+--
+
+
+
+--
+-- テーブルのインデックス ``
+--
+
+
+--
+-- ダンプしたテーブルの AUTO_INCREMENT
+--
+
+--
+-- テーブルの AUTO_INCREMENT ``
+--
+
+
+--
+-- テーブルの AUTO_INCREMENT ``
+--
+
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
