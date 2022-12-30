53413b0fd22d2e46e4fa24b200a584d83be5fa8f
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2022-12-30 20:55:15 +0900

    fix db.php

--- web/db.php
+++ web/db.php
@@ -1,26 +1,29 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 
-echo 'test';
+require_once('../vendor/autoload.php');
 
-require __DIR__ . '../../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
 
-function getDb(): PDO
-{
-  $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-  $dotenv->load();
-
+try {
   $dbDsn = $_ENV['DB_DSN'];
   $dbUser = $_ENV['DB_USER'];
   $dbPassword = $_ENV['DB_PASSWORD'];
 
-  $db = new PDO(
+  $pdo = new PDO(
     $dbDsn,
     $dbUser,
     $dbPassword,
     [
       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     ]
   );
-  return $db;
+} catch (PDOException $e) {
+  header('Content-Type: text/plain; charset=UTF-8', true, 500);
+  exit($e->getMessage());
 }
+
+header('Content-Type: text/html; charset=UTF-8');
