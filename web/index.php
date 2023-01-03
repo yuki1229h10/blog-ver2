cd786a4a2355c42ad15581c0b920e20d6a1e2d76
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2023-01-03 23:56:37 +0900

    add createNewUser() in index.php

--- web/index.php
+++ web/index.php
@@ -1,5 +1,7 @@
 <?php
 
+use Dotenv\Parser\Entry;
+
 class loginSystem
 {
   private $db_type = "mysql";
@@ -176,8 +178,7 @@ class loginSystem
       && strlen($_POST['user_name']) >= 2
       && preg_match('/^[a-z\d]{2,64}$/i', $_POST['user_name'])
       && !empty($_POST['user_email'])
-      && strlen($_POST['user_email']) <=
-      64
+      && strlen($_POST['user_email']) <= 64
       && filter_var($_POST['user_email'], FILTER_VALIDATE_EMAIL)
       && !empty($_POST['user_password_new'])
       && strlen($_POST['user_password_new']) >= 6
@@ -190,29 +191,35 @@ class loginSystem
     } elseif (empty($_POST['user_password_new']) || empty($_POST['user_password_repeat'])) {
       $this->feedback = "Empty Password";
     } elseif ($_POST['user_password_new'] !== $_POST['user_password_repeat']) {
-      $this->feedback = "Password and password repeat are not the same.";
+      $this->feedback = "Password and password repeat are not the same";
     } elseif (strlen($_POST['user_password_new']) < 6) {
-      $this->feedback = "Password has a minimum length of 6 characters.";
+      $this->feedback = "Password has a minimum length of 6 characters";
     } elseif (strlen($_POST['user_name']) > 64 || strlen($_POST['user_name']) < 2) {
-      $this->feedback = "Username cannot be shorter than 2 or longer than 64 characters.";
+      $this->feedback = "Username cannot be shorter than 2 or longer than 64 characters";
     } elseif (!preg_match('/^[a-z\d]{2,64}$/i', $_POST['user_name'])) {
-      $this->feedback = "Username does not fit the same scheme: only a-Z and numbers are allowed, 2 to 64 characters.";
-    }elseif(empty($_POST['user_email'])){
+      $this->feedback = "Username does not fit the name scheme: only a-Z and numbers are allowed, 2 to 64 characters";
+    } elseif (empty($_POST['user_email'])) {
       $this->feedback = "Email cannot be empty";
-    }elseif(strlen($_POST['user_email']) > 64){
-      $this->feedback = "Email cannot be longer than 64 characters.";
-    }elseif(!filter_var($_POST['user_email']) > FILTER_VALIDATE_EMAIL){
-      $this->feedback = "Email cannot be longer than 64 characters.";
-    }elseif
+    } elseif (strlen($_POST['user_email']) > 64) {
+      $this->feedback = "Email cannot be longer than 64 characters";
+    } elseif (!filter_var($_POST['user_email'], FILTER_VALIDATE_EMAIL)) {
+      $this->feedback = "Your email address is not in a valid email format";
+    } else {
+      $this->feedback = "An unknown error occurred.";
     }
-    }
-    }
-    }
+
+    // default return
+    return false;
+  }
+
+
+  private function createNewUser()
+  {
+    $user_name = htmlentities($_POST['user_name'], ENT_QUOTES);
+    $user_email = htmlentities($_POST['user_email'], ENT_QUOTES);
+    $user_password = $_POST['user_password_new'];
   }
 }
-
-
-
 
 // <?php
 // error_reporting(E_ALL);
